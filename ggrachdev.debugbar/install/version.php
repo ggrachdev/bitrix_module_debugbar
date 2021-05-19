195b8b4b5fb566e8d1a11a9e958a2416803ca06a
ggrachdev <ggrachdev@yandex.ru>
2021-05-19 14:49:34 +0400

    Change version_date and version

--- ggrachdev.debugbar/install/version.php
+++ ggrachdev.debugbar/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "0.0.5",
-    "VERSION_DATE" => "2021-03-04 10:00:00"
+    "VERSION" => "0.0.6",
+    "VERSION_DATE" => "2021-05-19 10:00:00"
 );
 ?>
