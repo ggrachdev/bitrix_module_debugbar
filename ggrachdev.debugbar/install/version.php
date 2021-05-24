7bd3f47cbbcd31a81d358cf80cfe13c620870c35
ggrachdev <ggrachdev@yandex.ru>
2021-05-24 12:52:10 +0400

    Add opportunity push module in local/modules

--- ggrachdev.debugbar/install/version.php
+++ ggrachdev.debugbar/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "0.0.6",
-    "VERSION_DATE" => "2021-05-19 10:00:00"
+    "VERSION" => "0.0.7",
+    "VERSION_DATE" => "2021-05-24 10:00:00"
 );
 ?>
