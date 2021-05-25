e59733a9d1858c7f9aaf90d8bdb6ef8a43a473c1
ggrachdev <ggrachdev@yandex.ru>
2021-05-25 12:52:23 +0400

    Add fluent interface filters.php

--- ggrachdev.debugbar/filters.php
+++ ggrachdev.debugbar/filters.php
@@ -1,60 +1,56 @@
 <?php
 
 DD()->addFilter('values', function ($data, $filterParams) {
-    if (\is_array($data)) {
-        return \array_values($data);
-    } else {
-        return $data;
-    }
-});
-
-DD()->addFilter('limit', function ($data, $filterParams) {
-    if (\is_array($data) && !empty($data)) {
-        if (empty($filterParams[0]) || !\is_numeric($filterParams[0]) || $filterParams[0] < 1) {
-            $count = 10;
+        if (\is_array($data)) {
+            return \array_values($data);
         } else {
-            $count = $filterParams[0];
+            return $data;
         }
-        $data = array_slice($data, 0, $count, true);
-    }
-
-    return $data;
-});
-
-DD()->addFilter('first', function ($data, $filterParams) {
-    if (\is_array($data) && !empty($data)) {
-        $data = array_shift($data);
-    }
-
-    return $data;
-});
-
-DD()->addFilter('keys', function ($data, $filterParams) {
-
-    if (
-        !empty($data) &&
-        is_array($data) &&
-        !empty($filterParams[0]) &&
-        \is_array($filterParams[0])
-    ) {
-        $newData = [];
-
-        foreach ($data as $k => $v) {
-            if (\in_array($k, $filterParams[0])) {
-                $newData[$k] = $v;
+    })
+    ->addFilter('limit', function ($data, $filterParams) {
+        if (\is_array($data) && !empty($data)) {
+            if (empty($filterParams[0]) || !\is_numeric($filterParams[0]) || $filterParams[0] < 1) {
+                $count = 10;
+            } else {
+                $count = $filterParams[0];
             }
+            $data = array_slice($data, 0, $count, true);
         }
 
-        $data = $newData;
-    }
+        return $data;
+    })
+    ->addFilter('first', function ($data, $filterParams) {
+        if (\is_array($data) && !empty($data)) {
+            $data = array_shift($data);
+        }
 
-    return $data;
-});
+        return $data;
+    })
+    ->addFilter('keys', function ($data, $filterParams) {
 
-DD()->addFilter('last', function ($data, $filterParams) {
-    if (\is_array($data) && !empty($data)) {
-        $data = array_pop($data);
-    }
+        if (
+            !empty($data) &&
+            is_array($data) &&
+            !empty($filterParams[0]) &&
+            \is_array($filterParams[0])
+        ) {
+            $newData = [];
 
-    return $data;
-});
+            foreach ($data as $k => $v) {
+                if (\in_array($k, $filterParams[0])) {
+                    $newData[$k] = $v;
+                }
+            }
+
+            $data = $newData;
+        }
+
+        return $data;
+    })
+    ->addFilter('last', function ($data, $filterParams) {
+        if (\is_array($data) && !empty($data)) {
+            $data = array_pop($data);
+        }
+
+        return $data;
+    });
