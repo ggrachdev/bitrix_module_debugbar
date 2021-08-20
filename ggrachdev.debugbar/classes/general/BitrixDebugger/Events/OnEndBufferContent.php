43fbb029bfcad6e23baf763e619205b57a1cc387
ggrach <ggrachdev@yandex.ru>
2021-08-20 17:29:08 +0400

    Update OnEndBufferContent.php - if debug not show in debug bar - debug bar not showed

--- ggrachdev.debugbar/classes/general/BitrixDebugger/Events/OnEndBufferContent.php
+++ ggrachdev.debugbar/classes/general/BitrixDebugger/Events/OnEndBufferContent.php
@@ -3,32 +3,36 @@
 namespace GGrach\BitrixDebugger\Events;
 
 use \Bitrix\Main\Application;
-    
+use GGrach\BitrixDebugger\Validator\ShowModeDebuggerValidator;
+
 /**
  * Description of OnEndBufferContent
  *
  * @author ggrachdev
  */
 class OnEndBufferContent {
-    
+
     public function addDebugBar(&$content) {
-        global $USER, $APPLICATION;
 
-        $request = Application::getInstance()->getContext()->getRequest();
+        if (ShowModeDebuggerValidator::needShowInDebugBar(DD()->getConfiguratorDebugger())) {
+            global $USER, $APPLICATION;
 
-        if (
-            strpos($APPLICATION->GetCurDir(), "/bitrix/") !== false ||
-            $APPLICATION->GetProperty("save_kernel") == "Y" ||
-            !\is_object($USER) ||
-            !$USER->IsAdmin() ||
-            $request->isAjaxRequest() ||
-            (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
-        ) {
-            return;
+            $request = Application::getInstance()->getContext()->getRequest();
+
+            if (
+                strpos($APPLICATION->GetCurDir(), "/bitrix/") !== false ||
+                $APPLICATION->GetProperty("save_kernel") == "Y" ||
+                !\is_object($USER) ||
+                !$USER->IsAdmin() ||
+                $request->isAjaxRequest() ||
+                (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest')
+            ) {
+                return;
+            }
+
+            $logData = \GGrach\BitrixDebugger\View\DebugBarView::render(DD());
+            $content = \preg_replace("~<\s*\t*/\s*\t*body\s*\t*>~", $logData . '</body>', $content);
         }
-
-        $logData = \GGrach\BitrixDebugger\View\DebugBarView::render(DD());
-        $content = \preg_replace("~<\s*\t*/\s*\t*body\s*\t*>~", $logData . '</body>', $content);
     }
 
 }
